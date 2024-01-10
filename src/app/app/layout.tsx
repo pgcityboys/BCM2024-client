96876e4ffe278ae5d12b9fb2c01ae516dbd14322
mrochny <tpalinski@icloud.com>
2024-01-10 18:08:19 +0100

    Complete session logic

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,16 +1,31 @@
 import { Navbar } from "@/components/app/Navbar"
 import { ReactNode } from "react"
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions} from "../api/auth/[...nextauth]/route";
+import { isNull } from "util";
 
-export default function Layout({children} : {
+const Layout = async ({children} : {
   children: ReactNode
-}) {
-  
+}) => {
+
+  const session = await getServerSession(authOptions)
+  if(isNull(session)){
+    redirect('/login')
+  }
+
   return (
-    <div className="flex justify-start items-center"> 
-      <Navbar/>
-      <main className="flex justify-center items-center flex-col w-full h-full">
-        {children}
-      </main>
-    </div>
+    <body>
+      <div className="flex justify-start items-center"> 
+        <Navbar/>
+        <main className="flex justify-center items-center flex-col w-full h-full">
+          {children}
+        </main>
+      </div>
+    </body>
   )
 }
+
+
+export default Layout;
+
